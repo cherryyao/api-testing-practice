6e9f60f71b595cf63a5a6d84b83c3383cf068ee1
YAOCH2 <christine.s.yao@oocl.com>
2018-07-30 11:25:02 +0800

    checkTheFirstRaceOf2014WasAtAlbertPark() pass

--- src/test/java/exercises/RestAssuredExercises1Test.java
+++ src/test/java/exercises/RestAssuredExercises1Test.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 
 public class RestAssuredExercises1Test {
@@ -92,7 +93,10 @@ public class RestAssuredExercises1Test {
         given().
                 spec(requestSpec).
                 when().
-                then();
+                get("/2014/1/circuits.json").
+                then().log().all().
+                assertThat().
+                body("MRData.CircuitTable.Circuits[0].circuitId",equalTo("albert_park"));
     }
 
     /***********************************************
